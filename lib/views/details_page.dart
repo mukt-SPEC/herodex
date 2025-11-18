7d54e0d402c08ac3956c73818724aa2c093a34e2
mukt-SPEC <alaminsad6@gmail.com>
2025-11-18 17:29:33 +0100

    working on details page

--- lib/views/details_page.dart
+++ lib/views/details_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:herodex/model/superhero.dart';
 import 'package:herodex/utilities/theme/app_theme.dart';
+import 'package:icons_plus/icons_plus.dart';
 
 class DetailsPage extends StatefulWidget {
   final SuperHero? superHero;
@@ -28,19 +29,100 @@ class _DetailsPageState extends State<DetailsPage> {
         ),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 8),
-        child: Column(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadiusGeometry.circular(24),
-              child: Image.network(
-                width: double.infinity,
-                widget.superHero!.images!.lg!,
+      body: Stack(
+        alignment: AlignmentGeometry.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 8),
+            child: Column(
+              spacing: 16,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadiusGeometry.circular(24),
+                  child: Image.network(
+                    width: double.infinity,
+                    widget.superHero!.images!.lg!,
+                  ),
+                ),
+                Text(
+                  widget.superHero!.biography!.fullName!,
+                  style: TextStyle(
+                    color: AppTheme.textPrimaryColor,
+                    fontSize: 24,
+                    fontFamily: GoogleFonts.schibstedGrotesk().fontFamily,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 40,
+            //width: MediaQuery.sizeOf(context).width - 48,
+            child: Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 0,
+                ),
+                child: Container(
+                  padding: EdgeInsets.all(8),
+
+                  decoration: BoxDecoration(
+                    color: AppTheme.appBarColor,
+                    borderRadius: BorderRadius.circular(48),
+                  ),
+                  child: Row(
+                    spacing: 8,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: AppTheme.scaffoldBgColor,
+                          borderRadius: BorderRadius.circular(48),
+                        ),
+                        child: Icon(Iconsax.user_bold, color: Colors.white),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          // color: AppTheme.scaffoldBgColor,
+                          borderRadius: BorderRadius.circular(48),
+                        ),
+                        child: Icon(Iconsax.graph_bold),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          // color: AppTheme.scaffoldBgColor,
+                          borderRadius: BorderRadius.circular(48),
+                        ),
+                        child: Icon(Iconsax.info_circle_bold),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          //color: AppTheme.scaffoldBgColor,
+                          borderRadius: BorderRadius.circular(48),
+                        ),
+                        child: Icon(Iconsax.emoji_normal_bold),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          // color: AppTheme.scaffoldBgColor,
+                          borderRadius: BorderRadius.circular(48),
+                        ),
+                        child: Icon(Iconsax.book_square_bold),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/widgets/nav_bar.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class AppNavBar extends StatelessWidget {
+  final Map<String, Icon> options;
+  final bool isSelected;
+  final int selected;
+  final ValueChanged onSelected;
+  const AppNavBar({
+    super.key,
+    required this.options,
+    required this.isSelected,
+    required this.selected,
+    required this.onSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
